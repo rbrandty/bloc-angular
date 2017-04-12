276d6b0cb9cf984fbc5817ba3e5ffadaa7178906
Rachel Brandt <rachel.brandt@gmail.com>
2017-04-12 15:36:28 -0400

    modify app/scripts/app.js and app/index.html for Assignment 3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,15 +5,20 @@
             enabled: true,
             requireBase: false
         });
-        
+
         $stateProvider
             .state('landing', {
             url: '/',
             templateUrl: '/templates/landing.html'
         })
-         .state('album', {
-             url: '/album',
-             templateUrl: '/templates/album.html'
+            .state('album', {
+            url: '/album',
+            templateUrl: '/templates/album.html'
+        })
+
+            .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
         });
     }
 
