6ea8f70e081f0d32c228ca0803b563f279b2e8e6
Rachel Brandt <rachel.brandt@gmail.com>
2017-04-13 15:14:48 -0400

    add/modify files for Angular checkpoint 5

---
+++ app/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,10 @@
+ (function() {
+     function LandingCtrl() {
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('LandingCtrl', LandingCtrl);
+            this.heroTitle = "Turn the Music Up!";
+
+ })();
