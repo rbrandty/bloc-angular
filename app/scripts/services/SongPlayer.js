1e9dc4d5d9fb3d909dd8c14deb15990b6225b844
Rachel Brandt <rachel.brandt@gmail.com>
2017-04-14 12:14:42 -0400

    modify SongPlayer.js for Angular assignment 7 - Services part 2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,12 +3,12 @@
         var SongPlayer = {};
 
         var currentSong = null;
-        /**
+ /**
  * @desc Buzz object audio file
  * @type {Object}
  */
         var currentBuzzObject = null;
-        /**
+ /**
  * @function setSong
  * @desc Stops currently playing song and loads new audio file as currentBuzzObject
  * @param {Object} song
@@ -27,6 +27,21 @@
             currentSong = song;
         };
 
+/**
+* @function playSong
+* @desc Plays currentBuzzObject and sets property of the song object to true
+* @param {Object} song
+*/
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
+        
+/**
+* @function SongPlayer.play
+* @desc Plays currentBuzzObject if currentBuzzObject is not the same as song object. Also plays currentBuzzObject if currentBuzzObject is the same as song object and currentBuzzObject is paused, and sets property of the song object to true
+* @param {Object} song
+*/       
         SongPlayer.play = function(song) {
 
             if (currentSong !== song) {
@@ -40,6 +55,11 @@
             }  
         };
 
+/**
+* @function SongPlayer.pause
+* @desc pauses currentBuzzObject and sets property of the song Object to false
+* @param {Object} song
+*/
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
